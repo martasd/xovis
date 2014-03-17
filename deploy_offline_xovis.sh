9f709408882de413c7309050a8ccd07fdec5beb5
Martin Dluhoš <martin@gnu.org>
2014-03-17 06:53:17 +0545

    Automated XOvis offline deployment with a bash script.

--- deploy_offline_xovis.sh
+++ deploy_offline_xovis.sh
@@ -1,56 +1,80 @@
 #!/bin/bash
 
-# Deploys XOvis app on a RHEL-based schoolserver from a USB stick
-# Usage: deploy_offline_xovis.sh
+# Deploys XOvis app on a RHEL-based schoolserver from a USB stick. Currently,
+# this script requires that Couch DB is running on a host from which XOvis can
+# be replicated
+#
+# Usage: deploy_offline_xovis.sh IP-ADDRESS
+#        IP-ADDRESS- ip address of the db source host
+#
+# Precondition: Change the deployment name in lib/main.js in XOvis Couch App
+#               before replicating
 
-# NOTE: Replicatation did not fully succeed
-# TODO: Automate everything
+# TODO: Use couchdb dump/restore instead of replication.
 
 RPMS="
-curl-7.19.7-37.el6_4.i686.rpm
 erlang-ibrowse-2.2.0-4.el6.i686.rpm
 erlang-mochiweb-1.4.1-5.el6.i686.rpm
 erlang-oauth-1.1.1-1.el6.i686.rpm
 js-1.70-12.el6.i686.rpm
 couchdb-1.0.4-2.el6.i686.rpm
-perl-TermReadKey-2.30-13.el6.i686.rpm
 python-pip-1.3.1-4.el6.noarch.rpm
 "
 
+SOURCE_IP=$1
 ROOT_DIR=`pwd`
 USER=admin
 PASS=admin
 CREDENTIALS=$USER:$PASS
 XOVIS_HOST_TARGET_INIT=127.0.0.1:5984
-XOVIS_HOST_TARGET=http://admin:admin@$XOVIS_HOST_TARGET_INIT
+XOVIS_HOST_TARGET=http://$CREDENTIALS@$XOVIS_HOST_TARGET_INIT
 DB_NAME=xovis
-XOVIS_DB=$XOVIS_HOST_TARGET_INIT/$DB_NAME
+DB_URL=$XOVIS_HOST_TARGET_INIT/$DB_NAME
 
 cd $ROOT_DIR/rpms
 rpm -i $RPMS
 echo "All RPMS are installed on the system: $RPMS."
+
+sed -i 's/^\(bind_address\s*=\s*\).*$/\10\.0\.0\.0/' /etc/couchdb/default.ini
+echo "Made couch application accessible from other hosts."
 
 service couchdb start
 chkconfig --add couchdb
 chkconfig couchdb on
 echo "Couch configured to start on boot"
 
-# Change the deployment name in lib/main.js before replicating
-# From localhost
-curl http://martasd:48951frei@localhost:5984/_replicate -H 'Content-Type: application/json' -d '{ "source": "xovis", "target": "http://admin:admin@172.18.96.1:5984/xovis" }'
-echo "Successfully replicated couch db files to the server."
-echo "Check that the database exists."
-# curl http://martasd:48951frei@localhost:5984/xovis
+DB_CREATED=$(curl -X PUT $DB_URL)
+ADMIN_ADDED=$(curl -X PUT $XOVIS_HOST_TARGET_INIT/_config/admins/$USER -d "\"$PASS\"")
+if [ "$DB_CREATED" == '{"ok":true}' ] && [ "$ADMIN_ADDED" == '']
+then
+    echo "Created a Couch db $DEPLOYMENT and added $USER to admins. Relax.:)"
+else
+    echo "Db creation $DEPLOYMENT failed or user $USER was not added as admin."
+    exit 1
+fi
 
-# Make sure to change bind_address in /etc/couchdb/default.ini to 0.0.0.0
 
-cd $ROOT_DIR/xo-stats
+# Needs to have Couch DB running on a host with SOURCE_IP
+DB_REPLICATED=$(curl $XOVIS_HOST_TARGET/_replicate -H 'Content-Type: application/json' -d  "{ \"source\": \"http://$SOURCE_IP:5984/deploy-xovis\", \"target\": \"$DB_NAME\" }")
+if [[ "$DB_REPLICATED" == "\"ok\":true" ]]
+then
+    echo "Successfully replicated XOvis Couch App to $XOVIS_HOST_TARGET/$DB_NAME."
+else
+    echo "Replication failed."
+    exit 1
+fi 
+
+
+cd $ROOT_DIR/python-packages
 pip install CouchDB-0.9.tar.gz
 pip install docopt-0.6.1.tar.gz
 echo "Installed Python requirements for xo-stats"
 
 echo "At the deployment site:"
-echo "First, copy back user backup data to /library/users after updating the server"
+echo "First, copy back user backup data to the server after updating it:"
+echo "rsync -rH <path-to-backup> /library/users"
 echo "Then, run the following command at the deployment site to insert existing\
 data into the database assuming you haven't changed default var values:"
-echo "./process_journal_stats.py dbinsert xovis --deployment $DEPLOYMENT --server $XOVIS_HOST_TARGET"
+echo "./process_journal_stats.py dbinsert $DB_NAME --deployment $DEPLOYMENT"
+
+echo "To manage Couch using browser dashboard go to: $XOVIS_HOST_TARGET/_utils"
